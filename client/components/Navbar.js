70a35359e7a58656eac4e71f670b08455a35cf50
Andrew Hsu <andrewhsu42@gmail.com>
2017-05-19 20:06:53 -0700

    (develop) Customize navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
 
 export default class Navbar extends React.Component {
-  handleSelect() {
+  handleSelect(selectedKey) {
     alert('selected ' + selectedKey);
   }
   render() {
     return (
-      <Nav bsStyle="tabs" activeKey={1} onSelect={this.handleSelect}>
-        <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
-        <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
-        <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
+      <Nav bsStyle="tabs" activeKey={1} onSelect={(a) => this.handleSelect(a)}>
+        <NavItem eventKey={1} title="Home" href="/">Home</NavItem>
+        <NavItem eventKey={2} title="About" href="/about">About</NavItem>
+        <NavItem eventKey={3} title="Portfolio" href="/portfolio">Portfolio</NavItem>
+        <NavItem eventKey={4} title="Contact" href="/contact" disabled>Contact</NavItem>
       </Nav>
     )
   }
