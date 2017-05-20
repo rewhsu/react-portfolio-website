44ba74f9f4bf4f3f04b55de452c2950efb94ba15
Andrew Hsu <andrewhsu42@gmail.com>
2017-05-19 20:02:17 -0700

    (develop) Implement working sample react bootstrap navbar

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Projects from './Projects';
+import Navbar from './Navbar';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         App
         <Projects />
       </div>
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Nav, NavItem } from 'react-bootstrap';
 
 export default class Navbar extends React.Component {
+  handleSelect() {
+    alert('selected ' + selectedKey);
+  }
   render() {
     return (
-      <div>Navbar</div>
+      <Nav bsStyle="tabs" activeKey={1} onSelect={this.handleSelect}>
+        <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
+        <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
+        <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
+      </Nav>
     )
   }
 }
