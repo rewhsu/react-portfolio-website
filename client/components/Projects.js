ddfae87360be53574d26de3cd8cea4c6de592493
Andrew Hsu <andrewhsu42@gmail.com>
2017-05-27 22:58:17 -0700

    (develop) Add public json data and use it to render all projects

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
-var sampleProjects = [
-  { id: 0,
-    name: 'First Project',
-    img: 'https://pbs.twimg.com/profile_images/856549987697733632/4s096MPz.jpg',
-  },
-  { id: 1,
-    name: 'Second Project',
-    img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/2015/12-december/151222-packers-pro-bowl-950.jpg',
-  },
-  { 
-    id: 2,
-    name: 'Third Project',
-    img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/2016/04-april/160414-schedule-release-PR-950.jpg',
-  },
-  { 
-    id: 3,
-    name: 'Fourth Project',
-    img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/common/150120-helmets-950.jpg',
-  },
-  { 
-    id: 4,
-    name: 'Fifth Project',
-    img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/2017/01-january/170118-young-players-feature-950.jpg',
-  },
-]
+// var sampleProjects = [
+//   { id: 0,
+//     name: 'First Project',
+//     img: 'https://pbs.twimg.com/profile_images/856549987697733632/4s096MPz.jpg',
+//   },
+//   { id: 1,
+//     name: 'Second Project',
+//     img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/2015/12-december/151222-packers-pro-bowl-950.jpg',
+//   },
+//   { 
+//     id: 2,
+//     name: 'Third Project',
+//     img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/2016/04-april/160414-schedule-release-PR-950.jpg',
+//   },
+//   { 
+//     id: 3,
+//     name: 'Fourth Project',
+//     img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/common/150120-helmets-950.jpg',
+//   },
+//   { 
+//     id: 4,
+//     name: 'Fifth Project',
+//     img: 'http://www.packers.com/assets/images/imported/GB/photos/article_images/2017/01-january/170118-young-players-feature-950.jpg',
+//   },
+// ]
+
+var sampleProjects = require('../../public/data/sampleProjects.json').projects;
 
 var SingleProject = ({ project }) => (
   <div>
