a6665657e29462aa7a0ca9a040885a6b1c3d3ea8
Andrew Hsu <andrewhsu42@gmail.com>
2017-05-27 22:22:27 -0700

    (fix) Refactor ImageCarousel

--- client/components/ImageCarousel.js
+++ client/components/ImageCarousel.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Slider from 'react-slick';
 
+var sampleImages = [
+  'https://pbs.twimg.com/profile_images/856549987697733632/4s096MPz.jpg',
+  'http://www.packers.com/assets/images/imported/GB/photos/article_images/2015/12-december/151222-packers-pro-bowl-950.jpg',
+  'http://www.packers.com/assets/images/imported/GB/photos/article_images/2016/04-april/160414-schedule-release-PR-950.jpg',
+  'http://www.packers.com/assets/images/imported/GB/photos/article_images/common/150120-helmets-950.jpg',
+  'http://www.packers.com/assets/images/imported/GB/photos/article_images/2017/01-january/170118-young-players-feature-950.jpg',
+]
+
 export default class ImageCarousel extends React.Component {
   render() {
     var settings = {
@@ -11,20 +19,19 @@ export default class ImageCarousel extends React.Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       initialSlide: 0,
+      centerMode: true,
     };
     var style = {
-      'marginLeft': 'auto',
-      'marginRight': 'auto',
+      width: auto;
+      margin: 0 auto;
     }
     return (
       <div>
         <h2>Go Pack!</h2>
         <Slider {...settings}>
-          <div><img style={style} src="https://pbs.twimg.com/profile_images/856549987697733632/4s096MPz.jpg"></img></div>
-          <div><img style={style} src="http://www.packers.com/assets/images/imported/GB/photos/article_images/2015/12-december/151222-packers-pro-bowl-950.jpg"></img></div>
-          <div><img style={style} src="http://www.packers.com/assets/images/imported/GB/photos/article_images/2016/04-april/160414-schedule-release-PR-950.jpg"></img></div>
-          <div><img style={style} src="http://www.packers.com/assets/images/imported/GB/photos/article_images/common/150120-helmets-950.jpg"></img></div>
-          <div><img style={style} src="http://www.packers.com/assets/images/imported/GB/photos/article_images/2017/01-january/170118-young-players-feature-950.jpg"></img></div>
+          {sampleImages.map((image) => (
+            <div><img style={style} src={image}></img></div>
+          ))}
         </Slider>
       </div>
     )
